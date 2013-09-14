5725c742378e3b528cae00187a338d544e282f9e
Eric Kelly <heroiceric@gmail.com>
2013-09-14 16:26:56 -0400

    Convert test helper to regular javascript
    I don't know why but I can't get it to work in Coffeescript

---
+++ test/javascripts/test_helper.js
@@ -0,0 +1,23 @@
+//= require application
+//= require_tree .
+//= require_self
+
+document.write('<div id="ember-testing-container"><div id="ember-testing">' +
+    '</div></div>');
+document.write('<style>#ember-testing-container { position: absolute; ' +
+    'background: white; bottom: 0; right: 0; width: 640px; height: 384px; ' +
+    'overflow: auto; z-index: 9999; border: 1px solid #ccc; } ' +
+    '#ember-testing { zoom: 50%; }</style>');
+
+App.rootElement = '#ember-testing';
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+App.setupForTesting();
+App.injectTestHelpers();
+
+function exists(selector) {
+  return !!find(selector).length;
+}
+
+function hasContent(content) {
+  return !!find('*:contains(' + content + ')').length;
+}
