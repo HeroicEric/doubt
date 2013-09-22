f481dcc210d6d6f71b5f1fda0d792a39f6658c6a
Eric Kelly <heroiceric@gmail.com>
2013-09-22 15:23:14 -0400

    Use ActiveModel Adapter and Serializer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,7 @@
 // for more details see: http://emberjs.com/guides/application/
 App = Ember.Application.create();
 
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend()
+App.ApplicationSerializer = DS.ActiveModelSerializer.extend()
+
 //= require_tree .
