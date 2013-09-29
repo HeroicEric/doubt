1ca64f7282ed27acc9606931b38c03d0807e99cd
Eric Kelly <heroiceric@gmail.com>
2013-09-28 20:01:58 -0400

    Update Checkpoint completion tests

--- test/models/checkpoint_completion_test.rb
+++ test/models/checkpoint_completion_test.rb
@@ -1,45 +1,31 @@
 require "test_helper"
 
 describe CheckpointCompletion do
-  before do
-    @checkpoint_completion = FactoryGirl.build(:checkpoint_completion)
+  subject { CheckpointCompletion.new }
+
+  it { must have_valid(:user).when(User.new) }
+  it { wont have_valid(:user).when(nil) }
+
+  it { must have_valid(:checkpoint).when(Checkpoint.new) }
+  it { wont have_valid(:checkpoint).when(nil) }
+
+  describe "uniqueness" do
+    it "can only have one completion per checkpoint, per user" do
+      subject.save
+      dupe_checkpoint = FactoryGirl.build(:checkpoint_completion,
+        user: subject.user,
+        checkpoint: subject.checkpoint)
+      dupe_checkpoint.wont_be :valid?
+    end
   end
 
   describe "associations" do
     it "belongs to a user" do
-      @checkpoint_completion.must_respond_to(:user)
+      subject.must_respond_to(:user)
     end
 
     it "belongs to a user" do
-      @checkpoint_completion.must_respond_to(:user)
-    end
-  end
-
-  context "with valid attributes" do
-    it "must be valid" do
-      @checkpoint_completion.must_be :valid?
-    end
-  end
-
-  describe "validations" do
-    it "requires a user" do
-      @checkpoint_completion.user = nil
-      @checkpoint_completion.wont_be :valid?
-    end
-
-    it "requires a checkpoint" do
-      @checkpoint_completion.checkpoint = nil
-      @checkpoint_completion.wont_be :valid?
-    end
-
-    describe "uniqueness" do
-      it "can only have one completion per checkpoint, per user" do
-        @checkpoint_completion.save
-        dupe_checkpoint = FactoryGirl.build(:checkpoint_completion,
-          user: @checkpoint_completion.user,
-          checkpoint: @checkpoint_completion.checkpoint)
-        dupe_checkpoint.wont_be :valid?
-      end
+      subject.must_respond_to(:user)
     end
   end
 end
